37d8dad4d90805d92a206125d95d777e70559f6c
vedant-agarwal-08 <105448072+vedant-agarwal-08@users.noreply.github.com>
2023-02-10 21:29:58 +0530

    doing ejs

--- app.js
+++ app.js
@@ -3,39 +3,26 @@
 const express= require("express");
 const bodyparser=require("body-parser");
 const app=express();
+var items=["Buy Food","Cook Food","Eat Food"];
 app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({extended:true}));
 app.get("/",function(req,res){
     var today=new Date();
-    var currentday=today.getDay();
-    var day="";
-    switch(currentday){
-        case 0:
-            day="Sunday";
-            break;
-        case 1:
-            day="Monday";
-            break;
-        case 2:
-            day="Tuesday";
-            break;
-        case 3:
-            day="Wednesday";
-            break;
-        case 4:
-            day="Thursday";
-            break;
-        case 5:
-            day="Friday";
-            break;
-        case 6:
-            day="Saturday";
-            break;
-        default:
-            console.log("Error:curent day is equal to:"+currentday);
-        
-    }
-    res.render("list",{kindofday:day})
+    var options={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
+      var day=today.toLocaleDateString("en-us",options);  
+    
+    res.render("list",{kindofday:day,newlistitems:items})
 });
+app.post("/",function(req,res){
+   var item=req.body.newItem;
+   items.push(item);
+    // console.log(item);
+    res.redirect("/");
+})
 app.listen(3000,function(){
     console.log("server started on port 3000");
 });
