e90a6bff3b8f95bea2baf2cce45d896d296772b6
vedant-agarwal-08 <105448072+vedant-agarwal-08@users.noreply.github.com>
2023-02-10 22:09:07 +0530

    working on ejs todolist

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app=express();
 var items=["Buy Food","Cook Food","Eat Food"];
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.get("/",function(req,res){
     var today=new Date();
     var options={
